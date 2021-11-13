453f36d4af03ceea594c218166287c8d7fed971a
KPaulina <41211513+KPaulina@users.noreply.github.com>
2021-11-13 19:13:49 +0000

    adding main

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from check.constants import WIDTH, HEIGHT
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Draughts')
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event. type  == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quite()
+
+
+main()
